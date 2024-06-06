612e51b241166da4252eabd6b15e9571ba7fb41d
leelapji <159124359+leelapji@users.noreply.github.com>
2024-06-06 14:52:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+import Navbar from './components/Navbar.jsx';
+import MainContent from './components/MainContent.jsx';
+import Footer from './components/Footer.jsx';
 
-function App() {
+const App = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div>
       <Navbar />
-      <MainContent className="flex-grow" />
+      <MainContent />
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
